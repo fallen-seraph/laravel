01f609423987cf4c8fe0991e796cb0928e656387
Aaron <fallenseriphan@gmail.com>
2016-05-25 22:44:14 -0600

    Creation of layout and index blade files

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DB;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class CardsController extends Controller
 {
-    //
+    public function index() {
+        return view('cards.index');
+    }
 }
