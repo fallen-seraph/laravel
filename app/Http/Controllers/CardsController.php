649b982d911248ddb4ddf770b00d2c6e9ef3aad4
Aaron <fallenseriphan@gmail.com>
2016-05-30 18:00:41 -0600

    CardsController adding Card class for DB

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-use DB;
+
+use \App\Card;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -9,7 +10,7 @@ use App\Http\Requests;
 class CardsController extends Controller
 {
     public function index() {
-        $cards = DB::table('cards')->get();
+        $cards Card::all();
         return view('cards.index', compact('cards'));
     }
 }
