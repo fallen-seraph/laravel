c3cc3a9bf10cab55ad712e1e25833a08f14a067b
Aaron <fallenseriphan@gmail.com>
2016-06-02 23:05:41 -0600

    updating controller

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Card;
+use App\Note;
 use App\Http\Requests;
 
 class NotesController extends Controller
 {
-    public function addNote(Request $request){
-        return $request->all();
+    public function addNote(Request $request, Card $card){
+        $note = new Note;
+        $note->body = $request->body;
+        $card->notes()->save($note);
+        return back();
     }
 }
