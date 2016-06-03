1bb80d02f8613e542a66383c9c3ff6ea7555dd06
Aaron <fallenseriphan@gmail.com>
2016-06-02 21:18:52 -0600

    updating Note to add fillable section

--- app/Note.php
+++ app/Note.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
+    protected $fillable = ['body'];
+    
     public function card(){
         return $this->belongsTo('App\Card');
     }
