f9ac765912fcc4d10086a2e9d2e7305413d90a5f
Aaron <fallenseriphan@gmail.com>
2016-06-02 23:15:58 -0600

    updating notes controller

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -10,9 +10,13 @@ use App\Http\Requests;
 class NotesController extends Controller
 {
     public function addNote(Request $request, Card $card){
-        $note = new Note;
-        $note->body = $request->body;
-        $card->notes()->save($note);
+//        $note = new Note;
+//        $note->body = $request->body;
+//        $card->notes()->save($note);
+
+        $card->notes()->create([
+            'body' => $request->body
+        ]);
         return back();
     }
 }
