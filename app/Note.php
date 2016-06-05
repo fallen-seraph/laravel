b906c2a9565271d859f0c1879278bf1c32661209
Aaron <fallenseriphan@gmail.com>
2016-06-05 00:48:59 -0600

    adding relationship

--- app/Note.php
+++ app/Note.php
@@ -7,8 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     protected $fillable = ['body'];
-    
+
     public function card(){
         return $this->belongsTo('App\Card');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
     }
 }
